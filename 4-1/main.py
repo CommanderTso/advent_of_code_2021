a6eb39955562f3a3107522e264f666e4c9c42971
Scott Macmillan <CommanderTso@users.noreply.github.com>
2022-07-05 14:36:39 -0400

    Solution for 4-1

---
+++ 4-1/main.py
@@ -0,0 +1,126 @@
+# I'm sure there are many cleaner and more elegant ways to do this!
+
+class Board:
+    def __init__(self):
+        self.lines = [[], [], [], [], []]
+        self.hits = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [
+            0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+    def __str__(self):
+        output = "Lines:\n"
+        for line in self.lines:
+            output_line = ""
+            for entry in line:
+                output_line += entry + " "
+            output += output_line.rstrip() + "\n"
+
+        output += "\nHits\n"
+        for line in self.hits:
+            output_line = ""
+            for entry in line:
+                output_line += str(entry) + " "
+            output += output_line.rstrip() + "\n"
+
+        return output
+
+    def check_number(self, pick):
+        for x in range(len(self.lines)):
+            for y in range(len(self.lines[x])):
+                if self.lines[x][y] == pick:
+                    self.hits[x][y] = 1
+                    return self.check_card()
+
+    def check_card(self):
+        for line in self.hits:
+            line_total = 0
+            for score in line:
+                line_total += score
+            if line_total == 5:
+                return True
+
+        y0_total = 0
+        y1_total = 0
+        y2_total = 0
+        y3_total = 0
+        y4_total = 0
+        for x in range(len(self.hits)):
+            y0_total += self.hits[x][0]
+            y1_total += self.hits[x][1]
+            y2_total += self.hits[x][2]
+            y3_total += self.hits[x][3]
+            y4_total += self.hits[x][4]
+
+        if y0_total == 5 or \
+                y1_total == 5 or \
+                y2_total == 5 or \
+                y3_total == 5 or \
+                y4_total == 5:
+            return True
+
+        if self.check_diagonals():
+            return True
+
+        return False
+
+    def check_diagonals(self):
+        line_total_1 = 0
+        line_total_2 = 0
+
+        for x in range(5):
+            line_total_1 += self.hits[x][x]
+            line_total_2 += self.hits[x][4-x]
+
+        if line_total_1 == 5 or line_total_2 == 5:
+            return True
+
+    def calculate_score(self, number):
+        print(f"Running calculate score! Number was {number}")
+        unmarked_total = 0
+
+        print(self.lines)
+        print(self.hits)
+
+        for x in range(len(self.hits)):
+            for y in range(len(self.hits[x])):
+                if self.hits[x][y] == 0:
+                    print(f"Got a hit for {x},{y}")
+                    unmarked_total += int(self.lines[x][y])
+                    print(f"unmarked_total is now {unmarked_total}")
+
+        return unmarked_total * int(number)
+
+
+def main():
+    calls = []
+    board_set = []
+
+    with open("data.txt", "r") as file:
+        board = Board()
+        counter = 0
+
+        for line in file:
+            if len(line) > 15:
+                calls = [number for number in line.rstrip().split(",")]
+            elif counter == 5:
+                board_set.append(board)
+                board = Board()
+                counter = 0
+            elif line == "\n":
+                pass
+            else:
+                board.lines[counter] = [number.lstrip()
+                                        for number in line.rstrip().split()]
+                counter += 1
+
+        board_set.append(board)
+
+    for number in calls:
+        for board in board_set:
+            if board.check_number(number):
+                print(board)
+                print(board.calculate_score(number))
+                return
+
+
+if __name__ == '__main__':
+    main()
